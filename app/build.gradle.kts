a83649b4ff15c2ff7b135a57d1992889cfacc644
adirahman <rahmanadi9@gmail.com>
2025-01-24 18:40:49 +0700

    basic UI, activity, fragment left and right

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt") // For Room annotations
 }
 
 android {
@@ -45,4 +46,19 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // RecyclerView and ViewPager2
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+
+    // Room Database
+    implementation("androidx.room:room-runtime:2.6.0")
+    kapt("androidx.room:room-compiler:2.6.0")
+
+    // Lifecycle (ViewModel & LiveData)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
+    // Coroutines for asynchronous tasks
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
--- app/src/main/java/com/arc/blchat/MainActivity.kt
+++ app/src/main/java/com/arc/blchat/MainActivity.kt
@@ -5,16 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.arc.blchat.fragments.LeftFragment
+import com.arc.blchat.fragments.RightFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val viewPager:ViewPager2 = findViewById(R.id.viewPager)
+        val adapter = ChatPagerAdapter(this)
+        viewPager.adapter = adapter
     }
+}
+
+class ChatPagerAdapter(activity: AppCompatActivity): FragmentStateAdapter(activity){
+    override fun getItemCount() = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return if(position == 0) LeftFragment() else RightFragment()
+    }
+
 }
